9531c34175a3366daffd312e2a9d86ce0a750dbe
Vishal Dubey <hdubey64@gmail.com>
2023-08-10 20:04:46 +0530

    Checkbox condtion

--- temperature_converter/assets/scripts.js
+++ temperature_converter/assets/scripts.js
@@ -3,6 +3,24 @@ let result = document.getElementById("result");
 let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
 
+// Adding an event listener to the "Celsius" checkbox
+celsius.addEventListener("click", function () {
+   // Checking if the "Celsius" checkbox is checked
+   if (celsius.checked) {
+      // If "Celsius" is checked, uncheck the "Fahrenheit" checkbox
+      fahrenheit.checked = false;
+   }
+});
+
+// Adding an event listener to the "fahrenheit" checkbox
+fahrenheit.addEventListener("click", function () {
+   // Checking if the "Fahrenheit" checkbox is checked
+   if (fahrenheit.checked) {
+      // If "Fahrenheit" is checked, uncheck the "Celsius"   checkbox
+      celsius.checked = false;
+   }
+});
+
 // Temperature converter function
 const convertUnit = () => {
    const unitIs = input.value;
